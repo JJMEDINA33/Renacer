583d20ead61f9ea738d8bd40077768481ff9166c
JJMEDINA33 <139028392+JJMEDINA33@users.noreply.github.com>
2024-05-07 11:23:45 -0500

    Update ChildrenRepositoryInterface.php
    Create Contract-Interfece

--- app/Repositories/Contracts/ChildrenRepositoryInterface.php
+++ app/Repositories/Contracts/ChildrenRepositoryInterface.php
@@ -1,1 +1,16 @@
+<?php
 
+namespace App\Repositories\Contracts;
+
+use Illuminate\Database\Eloquent\Collection;
+
+interface ChildrenRepositoryInterface {
+
+    public function store(string $name, string $lastName, int $age, string $schoolGrade, string $mother, string $father): void;
+
+    public function list(): Collection;
+
+    public function update($childId, string $name, string $lastName, int $age, string $schoolGrade, string $mother, string $father): void;
+
+    public function delete($childId): void;
+}
